b638653060f5cb61e4550c2af8ef998f4de8877d
Daniel Mejia <dmejia@econtainers.co>
2025-09-30 09:38:21 -0500

    Formulario reactivo para el login

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  loginForm!: FormGroup;
+
+   constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    remember: [false]
+  });
+   }
+
+   
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Data:', this.loginForm.value);
+    } else {
+      console.log('Formulario inválido');
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
 }
