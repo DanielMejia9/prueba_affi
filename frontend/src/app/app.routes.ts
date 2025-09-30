0c59256106e0ddc08ff4be1446fcc056ecc74abc
Daniel Mejia <dmejia@econtainers.co>
2025-09-30 09:28:39 -0500

    Ruta para el login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
+
+    {
+    path: '', 
+    component: Login,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+       { path: 'login', component: Login,},
+    ]
+  }
 ];
